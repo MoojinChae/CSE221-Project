a53233c5ffe771993d2837cd5e0553a05b0a4ca1
ChaeMoojin <Moojin@ChaeMoojins-MacBook-Pro.local>
2016-01-27 13:27:15 -0800

    refactoring indentation

--- CPU/kernel/kernel_module.c
+++ CPU/kernel/kernel_module.c
@@ -12,17 +12,16 @@ static void enable_ccnt_read(void* data){
 	asm volatile ("mcr p15, 0, %0, c9, c12, 1" :: "r"(1 << 31));
 }
 
-int hello_init(void)
-{
-  printk(KERN_ALERT "Hello kernel :)\n");
-	on_each_cpu(enable_ccnt_read, NULL, 1);
+int hello_init(void){
+	printk(KERN_ALERT "Hello kernel :)\n");
+  on_each_cpu(enable_ccnt_read, NULL, 1);
 	return 0;
 }
 
-void hello_exit(void)
-{
-    printk(KERN_ALERT "Goodbye World!\n");
+void hello_exit(void){
+	printk(KERN_ALERT "Goodbye World!\n");
 }
+
 module_init(hello_init);
 module_exit(hello_exit);
 
