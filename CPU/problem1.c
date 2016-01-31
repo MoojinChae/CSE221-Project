f3da0ecc69ec395f485bc3d52ae2a024e74c20dc
Jae Hyun Park <jhp099@cs.ucsd.edu>
2016-01-31 16:49:04 +0000

    Problem 1: Measure cycle counter reading time overhead and loop time overhead.

--- CPU/problem1.c
+++ CPU/problem1.c
@@ -1,7 +1,37 @@
 #include "cpu.h"
 #include <stdio.h>
 
-int main(){
-	float overhead = get_overhead();
-  return 0;
+#define READING_TIME_TEST_NUM 10000
+#define LOOP_TIME_TEST_NUM 10000
+
+unsigned reading_time_overhead();
+unsigned loop_time_overhead();
+
+unsigned reading_time_overhead() {
+    unsigned ccnt = 0;
+    unsigned total_ccnt = 0;
+
+    for (int i = 0; i < OVERHEAD_TEST_NUM; i++) {
+        ccnt = ccnt_read();
+        ccnt = ccnt_read() - ccnt;
+        total_ccnt += ccnt;
+    }
+
+    return total_ccnt / OVERHEAD_TEST_NUM;
+}
+
+unsigned loop_time_overhead() {
+    unsigned ccnt = 0;
+
+    ccnt = ccnt_read();
+    for (int i = 0; i < LOOP_TIME_TEST_NUM; i++) { }
+    ccnt = ccnt_read() - ccnt;
+
+    return ccnt / LOOP_TIME_TEST_NUM;
+}
+
+int main() {
+    printf("reading_time_overhead() : %u cycles\n", reading_time_overhead());
+    printf("loop_time_overhead() : %u cycles\n", loop_time_overhead());
+    return 0;
 }
