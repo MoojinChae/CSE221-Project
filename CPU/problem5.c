1c5c1ad5dca44d7935f989260a01596d76a9db7a
Jae Hyun Park <jhp099@cs.ucsd.edu>
2016-01-31 20:24:48 +0000

    Problem5: Add cycle count measurement for thread context switching time.

--- CPU/problem5.c
+++ CPU/problem5.c
@@ -2,22 +2,28 @@
 #include "cpu.h"
 #include <pthread.h>
 #include <stdio.h>
+#include <stdbool.h>
 
+#define CONTEXT_SWITCHING_NUM 100000
 void* mythread(void*);
 unsigned thread_context_switching_time();
 
-pthread_mutex_t p_mutex;
-pthread_cond_t cv;
+bool add_ccnt;
+unsigned ccnt;
+unsigned total = 0;
+int context_switch_num = 0;
 
 void* mythread (void* arg) {
-    //signal
-    printf("HERE!!\n");
-    pthread_cond_signal(&cv);
-    int pthread_count = 1000;
+    int diff = 0;
 
-    // MEASURE
-    for (int i = 0; i < 100; i++) {
-        printf("\n child_thread %lu, count = %d\n", pthread_self() % 1000, pthread_count++);
+    for (int i = 0; i < CONTEXT_SWITCHING_NUM; i++) {
+        diff = ccnt_read() - ccnt;
+        if (add_ccnt) {
+            total += diff;
+            context_switch_num++;
+            add_ccnt = false;
+        }
+        // printf("Child_thread %lu, ccnt = %u\n", pthread_self() % 1000, diff);
         pthread_yield();
     }
 
@@ -26,35 +32,26 @@ void* mythread (void* arg) {
 
 unsigned thread_context_switching_time() {
     pthread_t tid;
-    int pthread_id;
-    int main_count = 0;
-    int rc;
+    add_ccnt = false;
 
-    if (pthread_mutex_init(&p_mutex, NULL) != 0) {
-        printf("ERROR: mutex init failed\n");
-        return 1;
-    }
 
-    pthread_mutex_lock(&p_mutex);
-
-    pthread_id = pthread_create(&tid, NULL, mythread, NULL);
+    int pthread_id = pthread_create(&tid, NULL, mythread, NULL);
     if (pthread_id) {
         printf("ERROR: pthread_create error.\n");
         return 1;
     }
-    pthread_cond_wait(&cv, &p_mutex);
-    pthread_mutex_unlock(&p_mutex);
-    pthread_mutex_destroy(&p_mutex);
 
-    // MEASURE
-    for (int i = 0; i < 100; i++) {
-        printf("\n main_thread %lu, count = %d\n", pthread_self() % 1000, main_count++);
+    for (int i = 0; i < CONTEXT_SWITCHING_NUM; i++) {
+        add_ccnt = true;
+        ccnt = ccnt_read();
+        // printf("Main_thread %lu, ccnt = %u\n", pthread_self() % 1000, ccnt);
         pthread_yield();
     }
 
     pthread_join(tid, NULL);
-    printf("\n pthread done");
-    return 0;
+    unsigned average = total / context_switch_num;
+    printf("Average thread context switch time: %u\n", average);
+    return average;
 }
 
 int main() {
