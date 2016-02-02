ec2c44c9c4aa8e58ffa8de93f0f608116c21d58d
Jae Hyun Park <jhp099@cs.ucsd.edu>
2016-02-02 07:26:42 +0000

    Calculate pipe overhead to match our process context switch scenario.

--- CPU/pipe.c
+++ CPU/pipe.c
@@ -7,34 +7,32 @@
 #include <unistd.h>
 
 int main(){
-    int fd;
+    int fd[2];
     unsigned t;
     int num_iter = 100;
     int msg = 1;
 
-    fcntl(fd, F_SETPIPE_SZ, sizeof(msg));
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        return -1;
+    }
+
+    fcntl(fd[0], F_SETPIPE_SZ, sizeof(msg));
+    fcntl(fd[1], F_SETPIPE_SZ, sizeof(msg));
 
     unsigned overall_t = 0;
     for (int i = 1; i <= num_iter; i++){
         t = ccnt_read();
-        write(fd, &msg, sizeof(msg));
+        write(fd[1], &msg, sizeof(msg));
+        read(fd[0], &msg, sizeof(msg));
         t = ccnt_read() - t;
         //printf("%dth pipe write overhead : %f\n", i, (float)t);
         overall_t += t;
     }
     printf("average of pipe write overhead : %f\n", ((float)overall_t/num_iter));
 
-    overall_t = 0;
-    for (int i = 1; i <= num_iter; i++){
-        t = ccnt_read();
-        read(fd, &msg, sizeof(msg));
-        t = ccnt_read() - t;
-        //printf("%dth pipe read overhead : %f\n", i, (float)t);
-        overall_t += t;
-    }
-    printf("average of pipe read overhead : %f\n", ((float)overall_t/num_iter));
-
-    close(fd);
+    close(fd[0]);
+    close(fd[1]);
 
     return 0;
 }
