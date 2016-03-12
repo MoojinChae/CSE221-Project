c07d711d4795cd4761e5ad8330d0dd4d14f8e0ca
Jae Hyun Park <jhp099@cs.ucsd.edu>
2016-03-12 19:45:56 +0000

    Run 4-3 scripts.

---
+++ FileSystem/run_problem3_rand.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+./problem3_client $1 $2 $3 > nfs_rand_$1$2
+./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_rand_$1$2
---
+++ FileSystem/run_problem3_seq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./problem3_client $1 $2 $3 > nfs_seq_$1$2
+./problem3_client $1 $2 $3 >> nfs_seq_$1$2
+./problem3_client $1 $2 $3 >> nfs_seq_$1$2
+./problem3_client $1 $2 $3 >> nfs_seq_$1$2
+./problem3_client $1 $2 $3 >> nfs_seq_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_seq_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_seq_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_seq_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_seq_$1$2
+# ./problem3_client $1 $2 $3 >> nfs_seq_$1$2
