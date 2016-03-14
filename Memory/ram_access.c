edc9fe8ff59d8c96c62fa1cf7fd37b06fdb8090b
ChaeMoojin <chaesocool@gmail.com>
2016-03-14 11:26:26 -0700

    modify stride

--- Memory/ram_access.c
+++ Memory/ram_access.c
@@ -14,39 +14,36 @@ void measure_latency(float overhead){
     int arr_size, stride_size, int_arr_size, int_stride_size;
     void **measure;
 
-    for(int i=0; i<6; i++){
-        stride_size = stride_min_size * pow(2,i) - sizeof(int *);
-        printf("====== Stride Size : %d ======\n", stride_size);
-        for(int j=0; j<18; j++){
-            arr_size = arr_min_size * pow(2,j);
-            int ** array = (int **) malloc(arr_size);
+    for(int j=0; j<18; j++){
+        arr_size = arr_min_size * pow(2,j);
+        stride_size = arr_size/4 - sizeof(int *);
+        int ** array = (int **) malloc(arr_size);
 
-            int_arr_size = arr_size / sizeof(int *);
-            int_stride_size = stride_size / sizeof(int *);
-            for (int k = 0; k < int_arr_size; k++) {
-                array[k] = (int *) &array[(k + int_stride_size) % int_arr_size];
-            }
-
-            measure = (void **)array;
-            avg = 0.0;
-            stddev = 0.0;
-            min = 100000000;
-            max = 0;
-            for(int k=1; k<=LOOP_TIME_TEST_NUM; k++){
-                t = ccnt_read();
-                measure = *measure;
-                t = ccnt_read() - t;
-
-                float prev_avg = avg;
-                avg += (t - overhead - prev_avg) / k;
-                stddev += (t - overhead - prev_avg) * (t - overhead - avg);
-                if (t-overhead > max) max = t-overhead;
-                if (t-overhead < min) min = t-overhead;
-            }
-            stddev = sqrt(stddev / (LOOP_TIME_TEST_NUM - 1));
-            printf("arr size = %d : average = %f, std = %f, min = %d, max = %d\n", arr_size, avg, stddev, min, max);
-            free(array);
+        int_arr_size = arr_size / sizeof(int *);
+        int_stride_size = stride_size / sizeof(int *);
+        for (int k = 0; k < int_arr_size; k++) {
+            array[k] = (int *) &array[(k + int_stride_size) % int_arr_size];
         }
+
+        measure = (void **)array;
+        avg = 0.0;
+        stddev = 0.0;
+        min = 100000000;
+        max = 0;
+        for(int k=1; k<=LOOP_TIME_TEST_NUM; k++){
+            t = ccnt_read();
+            measure = *measure;
+            t = ccnt_read() - t;
+
+            float prev_avg = avg;
+            avg += (t - overhead - prev_avg) / k;
+            stddev += (t - overhead - prev_avg) * (t - overhead - avg);
+            if (t-overhead > max) max = t-overhead;
+            if (t-overhead < min) min = t-overhead;
+        }
+        stddev = sqrt(stddev / (LOOP_TIME_TEST_NUM - 1));
+        printf("arr size = %d, stride size = %d : average = %f, std = %f, min = %d, max = %d\n", arr_size, stride_size, avg, stddev, min, max);
+        free(array);
     }
     
 }
