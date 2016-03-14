0c06d73cc836a41dc2fc5df8ee488e036dd6a00b
Jae Hyun Park <jhp099@cs.ucsd.edu>
2016-03-14 02:45:07 +0000

    Modify problem5 process context switching.

--- CPU/problem5_process.c
+++ CPU/problem5_process.c
@@ -1,9 +1,11 @@
 #define _GNU_SOURCE
 #include "cpu.h"
 #include <fcntl.h>
+#include <math.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -14,7 +16,7 @@ int process_context_switching_time() {
     int pipefd[2];
     pid_t cpid;
     unsigned msg = 1;
-    unsigned ccnt = 0;
+    unsigned end_ccnt = 0;
     unsigned total = 0;
     int context_switch_num = 0;
 
@@ -26,7 +28,8 @@ int process_context_switching_time() {
     fcntl(pipefd[0], F_SETPIPE_SZ, sizeof(msg));
     fcntl(pipefd[1], F_SETPIPE_SZ, sizeof(msg));
 
-    // int start_ccnt = ccnt_read();
+    int start_ccnt = 0;
+
     if ((cpid = fork()) == -1) {
         perror("fork");
         return -1;
@@ -34,49 +37,41 @@ int process_context_switching_time() {
 
     if (cpid == 0) { // Child writes to pipe
         close(pipefd[0]);
-        for (int i = 0; i < CONTEXT_SWITCHING_NUM; i++) {
-            printf("Child\n");
-            msg = ccnt_read();
-            if (write(pipefd[1], &msg, sizeof(msg)) != sizeof(msg)) {
-                printf("1\n");
-                return -1;
-            }
-            sleep(1);
+        if (write(pipefd[1], &msg, sizeof(msg)) != sizeof(msg)) {
+            exit(1);
         }
-
-        return 0;
+        exit(1);
     } else { // Parent reads char to pipe
         close(pipefd[1]);
-        for (int i = 0; i < CONTEXT_SWITCHING_NUM; i++) {
-            if (read(pipefd[0], &msg, sizeof(msg)) != sizeof(msg)) {
-                printf("4\n");
-                return -1;
-            }
-            ccnt = ccnt_read();
-            printf("Parent: %u, %u\n", ccnt, msg);
-            if (ccnt > msg) {
-                printf("Added: %u\n", (ccnt - msg));
-                total += (ccnt - msg);
-                context_switch_num++;
-                msg = 4294967295;
-            }
+        start_ccnt = ccnt_read();
+        if (read(pipefd[0], &msg, sizeof(msg)) != sizeof(msg)) {
+            return -1;
         }
+        end_ccnt = ccnt_read();
         wait(NULL);
     }
 
-    // int end_ccnt = ccnt_read();
-    // int diff = end_ccnt - start_ccnt;
+    int diff = end_ccnt - start_ccnt;
     // printf("start: %u, end: %u, diff: %u\n", start_ccnt, end_ccnt, diff);
-    // printf("average: %u\n", diff / (CONTEXT_SWITCHING_NUM * 2));
-
-    printf("total: %u, context_switch_num: %u\n", total, context_switch_num);
-    unsigned average = total / context_switch_num;
-    printf("Average thread context switch time: %u\n", average);
-    return 0;
+    return diff;
 }
 
 int main() {
-    process_context_switching_time();
+    int count = 0;
+    double avg = 0.0;
+    double stddev = 0.0;
+    for (int i = 0; i < 1000; i++) {
+        int result = process_context_switching_time();
+        count++;
+        double prev_avg = avg;
+        result /= 2;
+        avg += (result - prev_avg) / count;
+        stddev += (result - prev_avg) * (result - avg);
+    }
+
+    stddev = sqrt(stddev / (count - 1));
+    printf("average: %lf , stddev: %lf\n", avg, stddev);
+
     return 0;
 }
 
