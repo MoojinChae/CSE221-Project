e240a4adf63caacca6c82d12ac9121ef010df544
Jae Hyun Park <jhp099@cs.ucsd.edu>
2016-03-14 01:46:35 +0000

    Change to use ns instead of cycles in measurement.

--- FileSystem/file_cache_size.c
+++ FileSystem/file_cache_size.c
@@ -2,12 +2,25 @@
 #include <fcntl.h>
 #include <math.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 int read_file_to_size(unsigned /* size */, bool);
+void timespec_subtract(struct timespec *start, struct timespec *end, struct timespec* diff);
+
+void timespec_subtract(struct timespec *start, struct timespec *end, struct timespec* diff) {
+    if ((end->tv_nsec - start->tv_nsec) < 0) {
+        diff->tv_sec = end->tv_sec - start->tv_sec - 1;
+        diff->tv_nsec = end->tv_nsec - start->tv_nsec + 1000000000;
+    } else {
+        diff->tv_sec = end->tv_sec - start->tv_sec;
+        diff->tv_nsec = end->tv_nsec - start->tv_nsec;
+    }
+}
 
 int read_file_to_size(unsigned size, bool warmup) {
     int fd;
@@ -26,8 +39,10 @@ int read_file_to_size(unsigned size, bool warmup) {
     unsigned start_time = 0;
     unsigned end_time = 0;
 
-    start_time = ccnt_read();
+    struct timespec start, end, time_diff;
+    // start_time = ccnt_read();
 
+    clock_gettime(CLOCK_MONOTONIC, &start);
     while (count < size) {
         if ((bytes_read = read(fd, buf, buf_size)) == -1) {
             perror("Error: read()");
@@ -35,14 +50,18 @@ int read_file_to_size(unsigned size, bool warmup) {
         }
         count += bytes_read;
     }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    timespec_subtract(&start, &end, &time_diff);
+    printf("INSTANT diff: %ld sec %lld ns, start time: %ld sec %lld ns, end time: %ld sec %lld ns\n", time_diff.tv_sec, (uint64_t)time_diff.tv_nsec, start.tv_sec, (uint64_t)start.tv_nsec, end.tv_sec, (uint64_t)end.tv_nsec);
 
-    end_time = ccnt_read();
+    // end_time = ccnt_read();
     /*
     if (!warmup)
         printf("size: %u start: %u, end: %u, diff = %u\n", size, start_time, end_time, end_time - start_time);
     */
 
-    return end_time - start_time;
+    // return end_time - start_time;
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
@@ -62,13 +81,13 @@ int main(int argc, char* argv[]) {
 
         for (int j = 0; j < 10; j++) {
             unsigned cycles = read_file_to_size(size, false);
-            prev_avg = avg;
-            avg += (cycles - prev_avg) / (j + 1);
-            stddev += (cycles - prev_avg) * (cycles - avg);
+            // prev_avg = avg;
+            // avg += (cycles - prev_avg) / (j + 1);
+            // stddev += (cycles - prev_avg) * (cycles - avg);
         }
 
-        stddev = sqrt(stddev / 9);
-        printf("size: %d, average = %lf, std = %lf, cyclepermb: %lf\n", size, avg, stddev, avg / i);
+        // stddev = sqrt(stddev / 9);
+        // printf("size: %d, average = %lf, std = %lf, cyclepermb: %lf\n", size, avg, stddev, avg / i);
     }
     return 0;
 }
